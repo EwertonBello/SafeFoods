20d33411d25c6a2f7b54b1295ff55972440f83ab
EwertonBello <ewerton.bello1212@gmail.com>
2021-06-04 20:53:02 -0300

    remove getAll of interface

---
+++ app/Repositories/CompanyRepository.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\Company;
+
+class CompanyRepository implements CompanyRepositoryInterface
+{
+    public function getValidCompanies()
+    {
+        Company::
+    }
+
+    public function getCompany($id)
+    {
+        // TODO: Implement getCompany() method.
+    }
+
+    public function myCompany()
+    {
+        // TODO: Implement myCompany() method.
+    }
+
+    public function updateCompany()
+    {
+        // TODO: Implement updateCompany() method.
+    }
+}
--- app/Repositories/CompanyRepositoryInterface.php
+++ app/Repositories/CompanyRepositoryInterface.php
@@ -1,15 +1,14 @@
 <?php
+
 namespace App\Repositories;
 
 interface CompanyRepositoryInterface{
 
-    public function getAll();
+    public function getValidCompanies();
 
     public function getCompany($id);
 
     public function myCompany();
-
-    public function getValidCompanies();
 
     public function updateCompany();
 
